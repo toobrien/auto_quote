d869744a15854ebbeb12610309d96ffb8cb2ede9
Taylor O'Brien <toobrien@amazon.com>
2024-04-24 20:38:31 -0700

    .

--- v2.js
+++ v2.js
@@ -5,7 +5,7 @@ const fs                    = require("node:fs");
 const IN_MAP                = {};
 
 
-// node v1.js 637533450 0.25 6 10 5 10000
+// node v2.js 637533450 0.25 6 10 5 10000
 
 
 // order
